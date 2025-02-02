2c53d0a5006c3b6739f71c2ef6963a95985d93d7
goatedpenguin <goatedpenguin74@gmail.com>
2025-02-02 00:02:29 -0800

    added some driver code/test cases

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 #include <stdint.h>
 #include "aliases.h"
 
-int main(){
-    
+int main() {
+    array* myArray;
+    create(&myArray, 5);
+
+    append(myArray, 0, 10);
+    append(myArray, 1, 20);
+    append(myArray, 2, 30);
+
+    // Print the array to see if elements were added correctly
+    for (int i = 0; i < myArray->used; i++) {
+        printf("%d ", myArray->data[i]);
+    }
+
+    destroy(myArray);  // Clean up
     return 0;
 }
